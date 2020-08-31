03a012166d24c9c12491f309af11fc08f69908d3
ruthjomo <ruthjomo19@gmail.com>
2020-08-31 15:01:03 +0300

    added a class ,a directive and date-count pipe

--- quote/src/app/app.module.ts
+++ quote/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { HighlightDirectiveDirective } from './highlight-directive.directive';
+import { DateCountPipe } from './date-count.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
     QuoteDetailComponent,
-    QuoteFormComponent
+    QuoteFormComponent,
+    HighlightDirectiveDirective,
+    DateCountPipe
   ],
   imports: [
     BrowserModule,
